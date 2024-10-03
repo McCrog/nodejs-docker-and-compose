240d48ddf89b89ae74ab808956e8314fba5072ed
McCrog <pit_mib@bk.ru>
2024-10-03 23:51:25 +0300

    tech(backend): enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,6 +8,7 @@ import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
 
