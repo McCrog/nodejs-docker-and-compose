ed5bf07eecb0f6339d337ea2f0005e2bed961505
McCrog <pit_mib@bk.ru>
2024-10-04 13:00:14 +0300

    tech(backend): update env example and update config names

--- backend/ormconfig.ts
+++ backend/ormconfig.ts
@@ -6,7 +6,7 @@ export const appDataSourceOptions: DataSourceOptions = <DataSourceOptions>{
   port: <number>Number(<string>process.env.POSTGRES_PORT),
   username: <string>process.env.POSTGRES_USER,
   password: <string>process.env.POSTGRES_PASSWORD,
-  database: <string>process.env.POSTGRES_DB_NAME,
+  database: <string>process.env.POSTGRES_DB,
   synchronize: <boolean>(process.env.POSTGRES_SYNCHRONIZE === 'true' || false),
   entities: [__dirname + '/**/**/*.entity.{js,ts}'],
 };
